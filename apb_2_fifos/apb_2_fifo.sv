8ee0ab5f72de1e699df18eb525b0e40e98d59afd
Rodrigo <rodrigo@example.com>
2025-03-27 10:51:39 +0100

    Refactor APB FIFO and checker: implement assertions for transfer stability, error generation, and handshake, while restructuring the interface for improved clarity and functionality.

--- apb_2_fifos/apb_2_fifo.sv
+++ apb_2_fifos/apb_2_fifo.sv
@@ -1,185 +1,147 @@
-module apb_2_fifo #(
-    parameter BASE_ADDR = 8,
-    parameter DATA_WIDTH = 32,
-    parameter DEPTH = 16    // power of 2
-) (
-    input  logic                      pclk,
-    input  logic                      presetn,
+`timescale 1us/1ns
 
-    // apb
-    input  logic [31:0]               paddr,
-    input  logic [2:0]                pprot,
-    input  logic                      psel,
-    input  logic                      penable,
-    input  logic                      pwrite,
-    input  logic [DATA_WIDTH-1:0]     pwdata,
-    input  logic [(DATA_WIDTH/8)-1:0] pstrb,
-    output logic                      pready,
-    output logic [DATA_WIDTH-1:0]     prdata,
-    output logic                      pslverr,
+interface apb_if #(parameter ADDR_WIDTH = 32, DATA_WIDTH = 32);
+    logic                       pclk;
+    logic [ADDR_WIDTH-1:0]      paddr;
+    logic [DATA_WIDTH-1:0]      pwdata;
+    logic [DATA_WIDTH-1:0]      prdata;
+    logic                       psel;
+    logic                       pwrite;
+    logic                       penable;
+    logic [2:0]                 pprot;
+    logic                       presetn;
+    logic [DATA_WIDTH/8-1:0]    pstrb;
+    logic                       pready;
+    logic                       pslverr;
+endinterface
 
-    // fifo out tx
-    input  logic                      read_fifo_tx,
-    output logic                      empty_tx,
-    output logic                      almost_empty_tx,
-    output logic [DATA_WIDTH-1:0]     fifo_r_data_tx,
+module apb_checker #(ADDR_MIN = 0, ADDR_MAX = 8) (apb_if apb);
+  // Aserción: Verifica que el inicio de transferencia se realice correctamente.
+  property apb_transfer_start;
+    @(posedge apb.pclk) disable iff (!apb.presetn)
+      (apb.psel && !apb.penable) |=> (apb.psel && apb.penable);
+  endproperty
 
-    // fifo in rx
-    input  logic                      write_fifo_rx,
-    output logic                      full_rx,
-    output logic                      almost_full_rx,
-    input  logic [DATA_WIDTH-1:0]     fifo_w_data_rx
-);
+  // Aserción: Verifica la generacion de error en pslverr cuando se accede a una dirección fuera del rango permitido.
+  property apb_error_gen;
+    @(posedge apb.pclk) disable iff (!apb.presetn)
+      (apb.psel && apb.penable && apb.pready && ((apb.paddr < ADDR_MIN) || (apb.paddr > ADDR_MAX))) |-> (apb.pslverr);
+  endproperty
 
-    localparam ADDR_READ_TX_STATUS  = 2'b00;
-    localparam ADDR_WRITE_TX        = 2'b00;
-    localparam ADDR_READ_RX_STATUS  = 2'b01;
-    localparam ADDR_READ_RX_DATA    = 2'b10;
+  // Aserción: Verifica que pslverr se active solo cuando se accede a una dirección fuera del rango permitido.
+  property apb_pslverr_check;
+    @(posedge apb.pclk) disable iff (!apb.presetn)
+      (apb.pslverr == ((apb.psel && apb.penable && (apb.paddr < ADDR_MIN || apb.paddr > ADDR_MAX)) ? 1'b1 : 1'b0));
+  endproperty
 
-    localparam N_REGS_ADDR          = 3;
+  // Aserción: Verifica la estabilidad de la dirección y la señal de control de escritura durante la transferencia.
+  property apb_signals_stability;
+    @(posedge apb.pclk) disable iff (!apb.presetn)
+      (apb.psel && apb.penable) |-> (apb.paddr == $past(apb.paddr)) && (apb.pwrite == $past(apb.pwrite));
+  endproperty
 
+  // Aserción: Verifica que, una vez iniciada la transferencia, la señal pready se active en un tiempo razonable.
+  property apb_ready_handshake;
+    @(posedge apb.pclk) disable iff (!apb.presetn)
+      (apb.psel && apb.penable) |=> ##[1:$] apb.pready;
+  endproperty
 
+  // Aserción: Verifica que penable no se active sin que psel esté activo.
+  property penable_without_psel;
+    @(posedge apb.pclk) disable iff (!apb.presetn)
+      (!apb.psel) |=> !apb.penable;
+  endproperty
 
-    logic[DATA_WIDTH-1:0]       regs_tx[DEPTH-1:0];
-    logic[$clog2(DEPTH)-1:0]    index_write_tx;
-    logic[$clog2(DEPTH)-1:0]    index_read_tx;
-    logic[$clog2(DEPTH):0]      size_fifo_tx;
-    logic                       almost_full_tx;
-    logic                       full_tx;
-    logic                       write_fifo_tx;
+  // Instanciación de las aserciones
+  a_transfer_start: assert property(apb_transfer_start)
+    else begin
+      $error("ERROR: Secuencia de inicio de transferencia APB fallida.");
+      $stop;
+    end 
 
-
-    logic[DATA_WIDTH-1:0]       regs_rx[DEPTH-1:0];
-    logic[$clog2(DEPTH)-1:0]    index_write_rx;
-    logic[$clog2(DEPTH)-1:0]    index_read_rx;
-    logic[$clog2(DEPTH):0]      size_fifo_rx;
-    logic                       almost_empty_rx;
-    logic                       empty_rx;
-    logic                       read_fifo_rx;
-
-    logic [3:0] reg_addr;
-
-    /*******************      RX      *********************/
-    // Logica escritura fifo rx
-    always_ff @(posedge pclk or negedge presetn) begin
-        if (!presetn) begin
-            index_write_rx <= 0;
-            for (int i=0; i<DEPTH; ++i)
-                regs_rx[i] <= 0;
-        end else begin
-            if (write_fifo_rx & ~full_rx) begin
-                index_write_rx <= index_write_rx + 1;
-                regs_rx[index_write_rx] <= fifo_w_data_rx;
-            end
-        end
+  a_signals_stability: assert property(apb_signals_stability)
+    else begin
+      $error("ERROR: Inestabilidad en las señales APB durante la transferencia.");
+      $stop;
     end
 
-    // upodate index read fifo rx
-    always_ff @(posedge pclk or negedge presetn) begin
-        if (!presetn) begin
-            index_read_rx <= 0;
-        end else begin
-            if (read_fifo_rx & ~empty_rx) begin
-                index_read_rx <= index_read_rx + 1;
-            end
-        end
+  a_ready_handshake: assert property(apb_ready_handshake)
+    else begin 
+      $error("ERROR: Handshake de pready no cumplido en la transferencia APB.");
+      $stop;
     end
 
-    // update size fifo rx
-    always_ff @(posedge pclk or negedge presetn) begin
-        if (!presetn) begin
-            size_fifo_rx <= DEPTH;
-        end else begin
-            case ({write_fifo_rx & ~full_rx, read_fifo_rx & ~empty_rx})
-                2'b10: size_fifo_rx <= size_fifo_rx - 1;
-                2'b01: size_fifo_rx <= size_fifo_rx + 1;
-                default: size_fifo_rx <= size_fifo_rx;
-            endcase        
-        end
+  a_penable_without_psel: assert property(penable_without_psel)
+    else begin
+      $error("ERROR: PENABLE activo sin PSEL en APB.");
+      $stop;
     end
 
-    // calculate fifo size rx
-    always_comb begin
-        full_rx = size_fifo_rx == 0;
-        almost_full_rx = size_fifo_rx == 1;
-        empty_rx = size_fifo_rx == DEPTH;
-        almost_empty_rx = size_fifo_rx == DEPTH - 1;
+  a_error_gen: assert property(apb_error_gen)
+    else begin
+      $error("ERROR: Generación de error en PSLVERR fallida.");
+      $stop;
     end
-    
-    // enable read fifo rx
-    always_comb read_fifo_rx = psel & ~pwrite & penable & ~pslverr & (reg_addr[3:2] == ADDR_READ_RX_DATA);
-
-    /*******************      TX      *********************/
-    // Logica escritura APB a fifo tx
-    always_ff @(posedge pclk or negedge presetn) begin
-        if (!presetn) begin
-            index_write_tx <= 0;  
-            for (int i=0; i<DEPTH; ++i)
-                regs_tx[i] <= 0;
-        end else if (write_fifo_tx & ~full_tx) begin
-            index_write_tx <= index_write_tx + 1;
-            regs_tx[index_write_tx] <= pwdata;
-        end
-    end
-
-    // Logica lectura fifo tx
-    always_ff @(posedge pclk or negedge presetn) begin
-        if (!presetn) begin
-            index_read_tx <= 0;
-        end else if (read_fifo_tx & ~empty_tx) begin
-            index_read_tx <= index_read_tx + 1;
-        end
-    end
-
-    // update size fifo tx
-    always_ff @(posedge pclk or negedge presetn) begin
-        if (!presetn) begin
-            size_fifo_tx <= DEPTH;
-        end else begin
-            case ({write_fifo_tx & ~full_tx, read_fifo_tx & ~empty_tx})
-                2'b10: size_fifo_tx <= size_fifo_tx - 1;
-                2'b01: size_fifo_tx <= size_fifo_tx + 1;
-                default: size_fifo_tx <= size_fifo_tx;
-            endcase        
-        end
-    end
-
-    // calculate fifo size tx
-    always_comb begin
-        full_tx = size_fifo_tx == 0;
-        almost_full_tx = size_fifo_tx == 1;
-        empty_tx = size_fifo_tx == DEPTH;
-        almost_empty_tx = size_fifo_tx == DEPTH - 1;
-    end
-
-    // read data from fifo tx
-    always_comb fifo_r_data_tx = regs_tx[index_read_tx];
-
-    // enable write to fifo tx
-    always_comb write_fifo_tx = psel & pwrite & penable & ~pslverr & (reg_addr[3:2] == ADDR_WRITE_TX);
-
-
-
-    /****************** APB *****************/
-    // Señal de transferencia realizada
-    always_comb pready = penable & psel;
-    // Error si la dirección está fuera de rango
-    always_comb pslverr = (paddr < BASE_ADDR || paddr > BASE_ADDR + 4*(N_REGS_ADDR-1)) & penable;
-    // calculate reg @drres base on paddr
-    always_comb reg_addr = paddr - BASE_ADDR;
-
-    // lectura apb
-    always_comb begin
-        if (penable && !pslverr && psel && !pwrite) begin
-            case (reg_addr[3:2])
-                ADDR_READ_TX_STATUS: prdata = {{28{1'b0}}, full_tx, almost_full_tx, empty_tx, almost_empty_tx};
-                ADDR_READ_RX_STATUS: prdata = {{28{1'b0}}, full_rx, almost_full_rx, empty_rx, almost_empty_rx};
-                ADDR_READ_RX_DATA:   prdata = regs_rx[index_read_rx];
-                default: prdata = 0;
-            endcase
-        end else begin
-            prdata = 0;
-        end
+  
+  a_pslverr_check: assert property(apb_pslverr_check)
+    else begin
+      $error("ERROR: PSLVERR no se activa correctamente.");
+      $stop;
     end
 
 endmodule
+
+
+class agent_APB_m;
+    // Virtual interface para acceder a las señales del bus APB
+    virtual apb_if apb_vif;
+
+    // Constructor que recibe el virtual interface
+    function new(virtual apb_if apb_vif);
+        this.apb_vif = apb_vif;
+        this.apb_vif.psel    = 0;
+        this.apb_vif.penable = 0;
+        this.apb_vif.pstrb = 4'hF;
+        this.apb_vif.paddr = 0;
+        this.apb_vif.pprot = 0;
+        this.apb_vif.pwdata = 0;
+        this.apb_vif.pwrite = 0;
+    endfunction
+
+    // Tarea para escribir datos en el bus APB
+    task write_APB_data(input bit [31:0] data, input bit [31:0] addr);
+        apb_vif.paddr   = addr;
+        apb_vif.pprot   = 0;
+        apb_vif.psel    = 1;
+        apb_vif.pwrite  = 1;
+        apb_vif.pwdata  = data;
+        apb_vif.penable = 0;
+        @(posedge apb_vif.pclk);
+        apb_vif.penable = 1;
+        @(posedge apb_vif.pclk);
+        wait(apb_vif.pready);
+        apb_vif.penable = 0;
+        apb_vif.psel    = 0;
+        apb_vif.pwrite  = 0;
+    endtask
+
+    // Tarea para leer datos del bus APB
+    task automatic read_APB_data(ref logic [31:0] data, input bit [31:0] addr);
+        apb_vif.paddr   = addr;
+        apb_vif.pprot   = 0;
+        apb_vif.psel    = 1;
+        apb_vif.pwrite  = 0;
+        apb_vif.penable = 0;
+        @(posedge apb_vif.pclk);
+        apb_vif.penable = 1;
+        @(posedge apb_vif.pclk);
+        wait(apb_vif.pready);
+        data = apb_vif.prdata;
+        apb_vif.penable = 0;
+        apb_vif.psel    = 0;
+        apb_vif.pwrite  = 0;
+    endtask
+
+
+
+endclass
